34702cdfc8d8d65df4664305348bab391a46d547
B1 <B1@B142>
2017-11-08 13:29:06 +0800

    [#152666250]

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -16,7 +16,8 @@ public class JavaApplication4 {
      */
     public static void main(String[] args) {
         System.out.print("hi");
-        System.out.print("haha");
+        System.out.print("hahalo");
+        
     }
     
 }
